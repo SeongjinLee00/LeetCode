faa0435428751a82c2d17bc1cadee6088079249a
SeongjinLee00 <powerseongjin@gmail.com>
2022-11-01 22:39:48 +0900

    Time: 87 ms (96.95%), Space: 14 MB (51.62%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,38 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        dr=[1,-1,0,0]
-        dc=[0,0,1,-1]
-        R=len(board)
-        C=len(board[0])
-        N=len(word)
-        K=False
-        
-        def dfs(r,c,n,visited):
-            nonlocal K
-            if n==0:
-                visited[r][c]=1
-            if n==N-1:
-                K=True
-                return
-
-            for i in range(4):
-                rr=r+dr[i]
-                cc=c+dc[i]
-                if 0<=rr<R and 0<=cc<C and board[rr][cc]==word[n+1] and not visited[rr][cc]:
-                    visited[rr][cc]=1
-                    dfs(rr,cc,n+1,visited)
-                    visited[rr][cc]=0
+        wordCounter = Counter(word)
+        boardCounter = Counter()
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                boardCounter[board[row][col]] += 1
             
-            return False
-        
-        
-        for i in range(R):
-            for j in range(C):
-                if board[i][j]==word[0]:
-                    dfs(i,j,0,[[0]*C for _ in range(R)])
-                    if K:return True
-        
+        for char in wordCounter:
+            if wordCounter[char] > boardCounter[char]:
+                return False   
+                
+        if wordCounter[word[0]] > wordCounter[word[-1]]:
+            word = word[::-1]
+            
+        def match(row, col, i):
+            if i == len(word):
+                return True
+            if row >= len(board) or row < 0 or col >= len(board[0]) or col < 0:
+                return False
+            returnVal = False
+            if board[row][col] == word[i]:
+                board[row][col] = "_"
+                if (
+                    match(row + 1, col, i + 1) or
+                    match(row - 1, col, i + 1) or
+                    match(row, col + 1, i + 1) or
+                    match(row, col - 1, i + 1)
+                ):
+                    returnVal = True
+                board[row][col] = word[i]
+            return returnVal
+            
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if match(row, col, 0):
+                    return True
         return False
